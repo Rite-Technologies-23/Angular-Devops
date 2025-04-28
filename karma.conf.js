93bd16faba23bbe20270af7c292d9d7f8a14e098
Dixita Kothari <155532001+dixitarite@users.noreply.github.com>
2025-04-28 13:31:50 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')  // Add the JUnit Reporter
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -22,7 +23,12 @@ module.exports = function (config) {
         { type: 'json-summary' }  // needed for GitHub Actions to enforce coverage thresholds
       ]
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    reporters: ['progress', 'kjhtml', 'coverage', 'junit'],  // Include junit reporter in the list of reporters
+    junitReporter: {
+      outputDir: 'test-results',  // Directory where the JUnit XML will be saved
+      outputFile: 'test-results.xml', // File name for the JUnit report
+      useBrowserName: false  // Avoid appending the browser name to the report
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
